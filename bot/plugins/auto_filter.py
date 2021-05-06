c1b57b5ff93f1901782d5e30286d2964f89bdb24
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-06 23:56:00 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -137,7 +137,7 @@ async def auto_filter(bot, update):
             InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
         ])
         
-                result[0].append([ InlineKeyboardButton(f"🔰 Channel🔰", url="https://t.me/joinchat/TV_lOjIzLBGmSMGi") ])
+                result[0].append([ InlineKeyboardButton(f"🔰 Channel🔰", url="https://t.me/DraXmovieZ") ])
         # if show_invite is True Append invite link buttons
         if show_invite:
             
