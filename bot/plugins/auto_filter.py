c85212b7385661fa256a6e463502abc0a438ca69
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-09 01:41:25 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -60,15 +60,17 @@ async def auto_filter(bot, update):
     filters = await db.get_filters(group_id, query)
     
     if filters:
-           results.append(
-
+        results.append(
                 [
-
-                    InlineKeyboardButton("channel", url="https://t.me/DraXMovieZ")
-
+                    InlineKeyboardButton("channel", url="https://t.me/MG_MEDIA")
                 ]
-
             )
+           
+
+               
+
+                    
+   
         for filter in filters: # iterating through each files
             file_name = filter.get("file_name")
             file_type = filter.get("file_type")
