7a36bf0dfbf44e2cfb95f148497d9b286b6d2969
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-09 00:38:15 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -11,43 +11,25 @@ db = Database()
 
 @Client.on_message(filters.command(["start"]) & filters.private, group=1)
 async def start(bot, update):
-        update_channel = Config.UPDATE_CHANNEL
-
+    update_channel = Config.UPDATE_CHANNEL
     if update_channel:
-
-    try:
-
+        try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
-
             if user.status == "kicked":
-
-            await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-
-            return
-
-    except UserNotParticipant:
-
+               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+               return
+        except UserNotParticipant:
             #await update.reply_text(f"Join @{update_channel} To Use Me")
-
             await update.reply_text(
-
                 text="**Join My Updates Channel to use ME 😎 🤭**",
-
                 reply_markup=InlineKeyboardMarkup([
-
                     [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
-
               ])
-
             )
-
             return
-
-    except Exception:
-
+        except Exception:
             await update.reply_text("Something Wrong. Contact my Support Group")
-
-            return
+            return    
     try:
         file_uid = update.command[1]
     except IndexError:
