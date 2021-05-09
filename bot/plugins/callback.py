babae083f61958f78da3a3122b661e14695ad33c
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-09 09:01:14 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,9 +142,9 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>😉 you got </i> <code>{leng}</code> <i>Results For Your Query👉 </i> <code>{query}</code>
+    text=f"<i>😉 you got </i> <code>{leng}</code> <i>Results For Your Query👉 </i> <code>{query}</code>"
          
-         "© DraX Movies",url="https://t.me/joinchat/TV_lOjIzLBGmSMGi" "
+         "© DraX Movies",url="https://t.me/joinchat/TV_lOjIzLBGmSMGi" 
         
     try:
         await update.message.edit(
