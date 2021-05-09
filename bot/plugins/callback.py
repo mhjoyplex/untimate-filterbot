6ae093cb118ab00b07df248ddffc0076ca3d20d6
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-09 14:29:15 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>😉 you got  </i> <code>{leng}</code> <i>Results For Your Query👉 </i> <code>{query}</code>Drax Movies"
+    text=f"<i>😉 you got  </i> <code>{leng}</code> <i>Results For Your Query👉 </i> <code>{query}</code>"
         
         
     try:
