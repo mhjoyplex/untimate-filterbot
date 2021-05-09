abf02f7cf428766e5db2d73e63b1b9811dc0cd48
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-09 13:40:58 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -189,7 +189,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"😉 you got{(len_results)} Results For Your Query👉 <code>{query}</code>",
+                text=f"😉 you got {(len_results)} Results For Your Query👉 <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
