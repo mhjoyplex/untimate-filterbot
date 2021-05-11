2e411d025bec2b7c84c24d3e93e0e51a952b71ed
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-11 18:07:20 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -131,6 +131,11 @@ async def auto_filter(bot, update):
             )
         
     else:
+        await bot.send_message(
+        chat_id = update.chat.id,
+        text=f"𝗖𝗼𝘂𝗹𝗱𝗻'𝘁 𝗳𝗶𝗻𝗱 𝘁𝗵𝗶𝘀 𝗺𝗼𝘃𝗶𝗲 😢ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടത്തി അതുപോലെ ഇവിടെ ടൈപ്പ് ചെയ്യുക....[Google the original name of the movie and type it here ....]",             
+        reply_to_message_id=update.message_id
+           )
         return # return if no files found for that query
     
 
